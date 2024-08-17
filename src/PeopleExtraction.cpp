511ad6dd52fd11eed56fc1155c500374c0d12f61
Jarek Karwowski <chromedivizer@gmail.com>
2024-08-17 23:37:02 +0200

    simplified data collection and processing, applied `people_msgs_utils::Person` to process data, reworked the launch file, general cleanup

--- src/PeopleExtraction.cpp
+++ src/PeopleExtraction.cpp
@@ -7,280 +7,260 @@
 
 #include "PeopleExtraction.hpp"
 
-#define DEBUG_TF
+#include <tf2/convert.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
-PeopleExtraction::PeopleExtraction() :
-	tf_listener_(tf_buffer_),
-	id_next_(0),
-	gazebo_tf_frame_(PeopleExtraction::GAZEBO_FRAME_ID_DEFAULT),
-	target_tf_frame_(PeopleExtraction::TARGET_FRAME_ID_DEFAULT),
-	callback_counter_(0),
-	callback_omits_(0),
-	seq_(0)
+#include <thread>
+
+PeopleExtraction::PeopleExtraction():
+	tf_listener_(tf_buffer_)
 {
-	std::string people_topic("");
-	std::string position_topic("");
-	nh_.param<std::string>("gazebo_frame", gazebo_tf_frame_, gazebo_tf_frame_);
-	nh_.param<std::string>("target_frame", target_tf_frame_, target_tf_frame_);
-	nh_.param<int>("callback_omits", callback_omits_, callback_omits_);
-	nh_.param<std::string>("people_topic", people_topic, "/people_topic");
-	nh_.param<std::string>("position_topic", position_topic, "/position_topic");
+	auto pub_frequency = nh_.param<double>("pub_frequency", 30.0);
+	nh_.param<std::string>("world_frame", world_tf_frame_, "world");
+	nh_.param<std::string>("target_frame", target_tf_frame_, "odom");
+
+	// model name check
 	nh_.getParam("model_name_patterns", people_name_patterns_);
 	if (!people_name_patterns_.size()) {
 		ROS_ERROR("PeopleExtraction - model_name_patterns is empty! Class would be ill-formed");
 		return;
 	}
-	sub_gazebo_ = nh_.subscribe<gazebo_msgs::ModelStates>("/gazebo/model_states",  1, &PeopleExtraction::gazeboModelStateCallback, this);;
-	pub_people_ = nh_.advertise<people_msgs::People>(people_topic, 5);
-	pub_pos_ = nh_.advertise<people_msgs::PositionMeasurementArray>(position_topic, 5);
+
+	// publishing frequency check
+	if (pub_frequency <= 0.0) {
+		ROS_ERROR(
+			"Simulator-based people data will not be published as wrong frequency was selected: %f Hz",
+			pub_frequency
+		);
+		return;
+	}
+
+	// create ROS interfaces
+	pub_people_ = nh_.advertise<people_msgs::People>("/people", 5);
+	pub_pos_ = nh_.advertise<people_msgs::PositionMeasurementArray>("/people_measurements", 5);
+	sub_gazebo_ = nh_.subscribe<gazebo_msgs::ModelStates>(
+		"/gazebo/model_states",
+		1,
+		&PeopleExtraction::gazeboModelStateCallback,
+		this
+	);
+
+	timer_pub_ = nh_.createTimer(
+		ros::Duration(1.0 / pub_frequency),
+		std::bind(
+			&PeopleExtraction::publish,
+			this
+		)
+	);
 }
 
-void PeopleExtraction::gazeboModelStateCallback(const gazebo_msgs::ModelStates::ConstPtr &msg) {
-	if (callback_counter_++ != callback_omits_) {
+void PeopleExtraction::gazeboModelStateCallback(const gazebo_msgs::ModelStates::ConstPtr& msg) {
+	if (msg->name.size() != msg->pose.size() || msg->name.size() != msg->twist.size()) {
+		ROS_ERROR("Vector sizes in the input message are not equal, cannot process further");
 		return;
 	}
 	std::lock_guard<std::mutex> lock(mutex_);
-	callback_counter_ = 0;
 
 	// update internal database dynamically
 	size_t models_found = 0;
-	for (auto pattern : people_name_patterns_) {
-		// evaluate, whether the object named with a given pattern exists in a simulation
-		std::vector<std::string> sim_names = findModels(msg->name, pattern);
-		models_found += sim_names.size();
-		// iterate over actual names
-		for (auto sim_name : sim_names) {
-			// check whether the person already exists in the internal database
-			if (!doesPersonExist(sim_name)) {
-				Person bud;
-				bud.name = sim_name;
-				people_data_.push_back(bud);
-				people_name_id_[sim_name] = id_next_++;
-				ROS_INFO("Gazebo Callback - Added new person '%s' (ID: %d), detected from naming pattern: '%s'", sim_name.c_str(), people_name_id_[sim_name], pattern.c_str());
-			}
+	size_t models_total = msg->name.size();
+	for (size_t i = 0; i < models_total; i++) {
+		// investigated object - obtain only the name at this stage
+		const auto& name = msg->name.at(i);
+		// check whether a model with a given name is of our interest (according to the naming patterns)
+		bool pattern_matched = false;
+		std::string pattern;
+		std::tie(pattern_matched, pattern) = isMatching(name, people_name_patterns_);
+		if (!pattern_matched) {
+			continue;
 		}
+
+		models_found++;
+		gazebo_msgs::ModelState model;
+		model.model_name = name;
+		model.pose = msg->pose.at(i);
+		model.twist = msg->twist.at(i);
+
+		// check whether the person already exists in the internal database (only need ModelState update then)
+		if (doesPersonExist(name)) {
+			people_gazebo_[name].second = model;
+			continue;
+		}
+		// new model - assign an ID and update its ModelState
+		people_gazebo_[name] = {people_gazebo_.size(), model};
+		ROS_INFO(
+			"Tracking a new person '%s' (ID: '%lu'), detected from naming pattern: '%s'",
+			people_gazebo_[name].second.model_name.c_str(),
+			people_gazebo_[name].first,
+			pattern.c_str()
+		);
 	}
+
 	// possibly delete renamed/destroyed objects
-	if (models_found < people_data_.size()) {
-		ROS_INFO("Seems that an object was deleted. Number of matching model names found: %lu, size of database: %lu",
-			models_found, people_data_.size());
+	if (models_found < people_gazebo_.size()) {
+		ROS_INFO(
+			"Seems that an object was deleted. Number of matching model names found: %lu, size of database: %lu",
+			models_found,
+			people_gazebo_.size()
+		);
 		deleteDestroyedPerson(msg->name);
 	}
-
-	// compute indexes of the `people_names` objects
-	for (auto&& person : people_data_) {
-		auto it = std::find(msg->name.begin(), msg->name.end(), person.name);
-		if (it != msg->name.end()) {
-			// found, compute index
-			int index = it - msg->name.begin();
-			// retrieve meaningful data from corresponding vectors
-			if (index >= 0) {
-				// fill up raw (Gazebo) values at first
-				person.pose.header.frame_id = gazebo_tf_frame_;
-				person.pose.header.seq++;
-				person.pose.header.stamp = ros::Time::now();
-
-				person.pose.pose.position = msg->pose.at(index).position;
-				person.pose.pose.orientation = msg->pose.at(index).orientation;
-
-				// copy header
-				person.vel.header = person.pose.header;
-
-				person.vel.twist.linear = msg->twist.at(index).linear;
-				person.vel.twist.angular = msg->twist.at(index).angular;
-				// try to transform to another coordinate system
-				try {
-					geometry_msgs::TransformStamped transform = tf_buffer_.lookupTransform(
-						target_tf_frame_,
-						gazebo_tf_frame_,
-						ros::Time::now(),
-						ros::Duration(1.0)
-					);
-					// transform pose
-					geometry_msgs::PoseStamped pose_backup = person.pose;
-					tf2::doTransform(person.pose, person.pose, transform);
-					#ifdef DEBUG_TF
-					printf("[transform pose (1)] before - position: %2.5f, %2.5f, %2.5f, orientation: %2.5f, %2.5f, %2.5f\r\n",
-						pose_backup.pose.position.x,
-						pose_backup.pose.position.y,
-						pose_backup.pose.position.z,
-						pose_backup.pose.orientation.x,
-						pose_backup.pose.orientation.y,
-						pose_backup.pose.orientation.z
-					);
-					printf("[transform pose (2)] after  - position: %2.5f, %2.5f, %2.5f, orientation: %2.5f, %2.5f, %2.5f\r\n",
-						person.pose.pose.position.x,
-						person.pose.pose.position.y,
-						person.pose.pose.position.z,
-						person.pose.pose.orientation.x,
-						person.pose.pose.orientation.y,
-						person.pose.pose.orientation.z
-					);
-					#endif
-					// rotate velocity (in the local coordinate system)
-					rotateTwist(person.vel, transform);
-					// update header frame
-					person.pose.header.frame_id = target_tf_frame_;
-					person.vel.header.frame_id = target_tf_frame_;
-					#ifdef DEBUG_TF
-					printf("Assuming successful transformation - changed frame_id to TARGET: %s\r\n",
-						person.pose.header.frame_id.c_str());
-					#endif
-				} catch (tf2::TransformException &e) {
-					ROS_WARN("exception: %s\r\nPerson %s: could not find TF from %s to %s",
-						e.what(), msg->name.at(index).c_str(), gazebo_tf_frame_.c_str(), target_tf_frame_.c_str());
-				}
-			}
-		}
-	}
-
-	// publishing section
-	publishPeople();
-	publishPeoplePositions();
 }
 
-bool PeopleExtraction::doesPersonExist(const std::string &name) const {
-	for (auto person : people_data_) {
-		if (person.name == name) {
-			return true;
-		}
-	}
-	return false;
+bool PeopleExtraction::doesPersonExist(const std::string& name) const {
+	return people_gazebo_.find(name) != people_gazebo_.cend();
 }
 
-std::vector<std::string> PeopleExtraction::findModels(const std::vector<std::string> &model_names, std::string pattern) const {
-	std::vector<std::string> db;
-	for (auto name : model_names) {
-		// find
-		if (name.find(pattern) != std::string::npos) {
-			// found
-			db.push_back(name);
+std::tuple<bool, std::string> PeopleExtraction::isMatching(const std::string& model_name, std::vector<std::string>& patterns) const {
+	for (const auto& pattern: patterns) {
+		// try to find
+		if (model_name.find(pattern) == std::string::npos) {
+			continue;
 		}
+		return {true, pattern};
 	}
-	return db;
+	return {false, std::string()};
 }
 
-void PeopleExtraction::deleteDestroyedPerson(const std::vector<std::string> &model_names) {
-	int num_deleted = 0;
-	auto i = std::begin(people_data_);
+void PeopleExtraction::deleteDestroyedPerson(const std::vector<std::string>& model_names) {
+	auto i = std::begin(people_gazebo_);
 	// while is safer than for here
-	while (i != std::end(people_data_)) {
-		auto it = std::find(model_names.begin(), model_names.end(), i->name);
+	while (i != std::end(people_gazebo_)) {
+		// map and nested pair
+		auto model_name = i->second.second.model_name;
+		auto it = std::find(model_names.begin(), model_names.end(), model_name);
 		if (it != model_names.end()) {
 			// found
 			i++;
 			continue;
 		}
 		// delete current element
-		ROS_INFO("Deleting '%s' from database", i->name.c_str());
-		// erase from map
-		num_deleted += people_name_id_.erase(i->name);
-		people_data_.erase(i);
+		ROS_INFO("Deleting '%s' from database", model_name.c_str());
+		// erase from map; ref: https://stackoverflow.com/a/2874533
+		i = people_gazebo_.erase(i);
 	}
-	ROS_INFO("Deleted %d object(s)", num_deleted);
 }
 
-void PeopleExtraction::rotateTwist(geometry_msgs::TwistStamped &vel, const geometry_msgs::TransformStamped &transform) {
-	// local coordinate system - consider rotation only
-	geometry_msgs::TransformStamped transform_local = transform;
-	transform_local.transform.translation.x = 0.0;
-	transform_local.transform.translation.y = 0.0;
-	transform_local.transform.translation.z = 0.0;
+people_msgs_utils::People PeopleExtraction::gazeboModelsToPeople() const {
+	// transform localization data to the desired frame
+	geometry_msgs::TransformStamped tf_stamped;
+	// by default - lack of translation and rotation
+	tf_stamped.transform.rotation.w = 1.0;
 
-	// use msg type supported by tf2::doTransform
-	// http://docs.ros.org/en/kinetic/api/tf2_geometry_msgs/html/c++/namespacetf2.html#a82ca47c6f5b0360e6c5b250dca719a78
-	geometry_msgs::Vector3Stamped vel_v3;
-	vel_v3.header = vel.header;
-	vel_v3.vector.x = vel.twist.linear.x;
-	vel_v3.vector.y = vel.twist.linear.y;
-	vel_v3.vector.z = vel.twist.linear.z;
-	#ifdef DEBUG_TF
-	printf("[rotateTwist (1)] transform - raw tr: %2.3f, %2.3f, %2.3f | modded tr: %2.3f, %2.3f, %2.3f | rot: %2.3f, %2.3f, %2.3f, %2.3f\r\n",
-		transform.transform.translation.x,
-		transform.transform.translation.y,
-		transform.transform.translation.z,
-		transform_local.transform.translation.x,
-		transform_local.transform.translation.y,
-		transform_local.transform.translation.z,
-		transform_local.transform.rotation.x,
-		transform_local.transform.rotation.y,
-		transform_local.transform.rotation.z,
-		transform_local.transform.rotation.w
-	);
+	// obtain the transform to the desired frame
+	try {
+		tf_stamped = tf_buffer_.lookupTransform(
+			target_tf_frame_,
+			world_tf_frame_,
+			ros::Time(0)
+		);
+	} catch (tf2::TransformException& ex) {
+		ROS_ERROR(
+			"Could not find a transform from the `%s` to `%s` frame! Exception details: `%s`",
+			world_tf_frame_.c_str(),
+			target_tf_frame_.c_str(),
+			ex.what()
+		);
+		// avoid flooding console with this kind of errors (localization might not be operational yet)
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		return people_msgs_utils::People();
+	}
 
-	tf2::Quaternion q_rot;
-	tf2::convert(transform_local.transform.rotation, q_rot);
-	tf2::Matrix3x3 mat(q_rot);
-	double roll, pitch, yaw;
-	mat.getRPY(roll, pitch, yaw);
-	printf("[rotateTwist (2)] transform - rot RPY: %2.3f, %2.3f, %2.3f\r\n", roll, pitch, yaw);
-	#endif
+	// prepare output container
+	people_msgs_utils::People people;
 
-	geometry_msgs::Vector3Stamped vel_v3_modified;
-	tf2::doTransform(vel_v3, vel_v3_modified, transform_local);
-	#ifdef DEBUG_TF
-	printf("[rotateTwist (3)] vel_before: %2.3f, %2.3f, %2.3f | vel_after: %2.3f, %2.3f, %2.3f\r\n",
-		vel_v3.vector.x,
-		vel_v3.vector.y,
-		vel_v3.vector.z,
-		vel_v3_modified.vector.x,
-		vel_v3_modified.vector.y,
-		vel_v3_modified.vector.z
-	);
-	#endif
+	for (auto const& model_data: people_gazebo_) {
+		// retrieve from the value (key is not considered here)
+		auto id = model_data.second.first;
+		auto model = model_data.second.second;
 
-	// copy meaningful values to the twist msg
-	vel.twist.linear.x = vel_v3_modified.vector.x;
-	vel.twist.linear.y = vel_v3_modified.vector.y;
-	vel.twist.linear.z = vel_v3_modified.vector.z;
-	// just to be on the safe side
-	vel.twist.angular.x = 0.0;
-	vel.twist.angular.y = 0.0;
-	vel.twist.angular.z = 0.0;
+		geometry_msgs::PoseWithCovariance pose;
+		pose.pose = model.pose;
+		pose.covariance.fill(0.0); // assuming ideal data
+		// convert data from twist to pose representation (contents are the same)
+		geometry_msgs::PoseWithCovariance velocity;
+		velocity.covariance.fill(0.0); // assuming ideal data
+		velocity.pose.position.x = model.twist.linear.x;
+		velocity.pose.position.y = model.twist.linear.y;
+		velocity.pose.position.z = model.twist.linear.z;
+		tf2::Quaternion angular_vel;
+		// NOTE: for a proper yaw angle, yaw, pitch and roll angles must be reordered (compared to the documentation)
+		angular_vel.setEuler(
+			model.twist.angular.x,
+			model.twist.angular.y,
+			model.twist.angular.z
+		);
+		velocity.pose.orientation = tf2::toMsg(angular_vel);
+
+		// create a Person instance (with geom. data in the source frame)
+		people_msgs_utils::Person p(
+			std::to_string(id), // avoid using 'string'-type names here
+			pose,
+			velocity,
+			1.0, // complete reliability
+			false,
+			true,
+			id,
+			ros::Time::now().toNSec(), // track age = since startup as we have ideal data
+			std::string("") // group data not included
+		);
+		// TODO: include group data (obtain from node parameters)
+
+		// transform to the target frame
+		p.transform(tf_stamped);
+		// collect
+		people.push_back(p);
+	}
+
+	return people;
 }
 
-void PeopleExtraction::publishPeople() {
+void PeopleExtraction::publish() {
+	std::lock_guard<std::mutex> lock(mutex_);
+
+	// check if there is something to publish
+	if (people_gazebo_.empty()) {
+		return;
+	}
+
+	// Gazebo models to people representation
+	auto people = gazeboModelsToPeople();
+
+	publishPeople(people);
+	publishPeoplePositions(people);
+}
+
+void PeopleExtraction::publishPeople(const people_msgs_utils::People& people) {
 	people_msgs::People people_msg;
-	people_msgs::Person person_msg;
 
-	people_msg.header.frame_id = gazebo_tf_frame_;
-	people_msg.header.seq = seq_;
+	people_msg.header.frame_id = target_tf_frame_;
 	people_msg.header.stamp = ros::Time::now();
-	for (auto person : people_data_) {
-		// http://docs.ros.org/en/kinetic/api/people_msgs/html/msg/Person.html
-		person_msg.name = person.name;
-		person_msg.position = person.pose.pose.position;
-		person_msg.velocity.x = person.vel.twist.linear.x;
-		person_msg.velocity.y = person.vel.twist.linear.y;
-		person_msg.velocity.z = person.vel.twist.linear.z;
-		person_msg.reliability = 1.0;
-		people_msg.people.push_back(person_msg);
+	for (const auto& person: people) {
+		people_msg.people.push_back(person.toPersonStd());
 	}
 	pub_people_.publish(people_msg);
 }
 
-void PeopleExtraction::publishPeoplePositions() {
-	people_msgs::PositionMeasurementArray position_measurement_array_msg;
-	people_msgs::PositionMeasurement position_measurement_msg;
+void PeopleExtraction::publishPeoplePositions(const people_msgs_utils::People& people) {
+	people_msgs::PositionMeasurementArray pos_msg_array;
 
-	position_measurement_array_msg.header.frame_id = gazebo_tf_frame_;
-	position_measurement_array_msg.header.seq = seq_;
-	position_measurement_array_msg.header.stamp = ros::Time::now();
+	pos_msg_array.header.frame_id = target_tf_frame_;
+	pos_msg_array.header.stamp = ros::Time::now();
 
-	position_measurement_msg.header = position_measurement_array_msg.header;
+	for (auto person: people) {
+		people_msgs::PositionMeasurement pos_msg;
+		pos_msg.header = pos_msg_array.header;
 
-	for (auto person : people_data_) {
-		// http://docs.ros.org/en/kinetic/api/people_msgs/html/msg/PositionMeasurement.html
-		position_measurement_msg.name = person.name;
-		position_measurement_msg.object_id = person.object_id;
-		position_measurement_msg.pos.x = person.pose.pose.position.x;
-		position_measurement_msg.pos.y = person.pose.pose.position.y;
-		position_measurement_msg.pos.z = person.pose.pose.position.z;
-		position_measurement_msg.reliability = 1.0;
+		pos_msg.name = person.getName();
+		pos_msg.object_id = person.getName();
+		pos_msg.pos.x = person.getPositionX();
+		pos_msg.pos.y = person.getPositionY();
+		pos_msg.pos.z = person.getPositionZ();
+		pos_msg.reliability = person.getReliability();
 		// measurements are ideal, so covariance = 0
 		// FIXME: initialization? what is that for?
-		position_measurement_msg.initialization = 0;
-		position_measurement_array_msg.people.push_back(position_measurement_msg);
+		pos_msg.initialization = 0;
+		pos_msg_array.people.push_back(pos_msg);
 	}
-	pub_pos_.publish(position_measurement_array_msg);
+	pub_pos_.publish(pos_msg_array);
 }
--- src/PeopleExtraction.hpp
+++ src/PeopleExtraction.hpp
@@ -8,24 +8,24 @@
 #pragma once
 
 #include <ros/ros.h>
-#include <people_msgs/People.h>
-#include <people_msgs/PersonStamped.h>
-#include <people_msgs/PositionMeasurementArray.h>
+
+#include <gazebo_msgs/ModelState.h>
 #include <gazebo_msgs/ModelStates.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/TwistStamped.h>
+
+#include <people_msgs/People.h>
+#include <people_msgs/PersonStamped.h>
+#include <people_msgs/PositionMeasurementArray.h>
+#include <people_msgs_utils/person.h>
+
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <mutex>
 
-#include "Person.hpp"
+#include <mutex>
 
 class PeopleExtraction {
 public:
-	static constexpr auto GAZEBO_FRAME_ID_DEFAULT = "world";
-	static constexpr auto TARGET_FRAME_ID_DEFAULT = "map";
-
 	PeopleExtraction();
 	virtual ~PeopleExtraction() = default;
 
@@ -33,41 +33,42 @@ private:
 	/**
 	 * @brief
 	 * @note This is a high frequency topic
-	 * @note TODO: message_filters, TimeSynchronizer?
 	 * @param msg
 	 */
-	void gazeboModelStateCallback(const gazebo_msgs::ModelStates::ConstPtr &msg);
+	void gazeboModelStateCallback(const gazebo_msgs::ModelStates::ConstPtr& msg);
 
 	/**
 	 * @brief Evaluates, whether the person exists in the internal database
 	 * @param name: name of the person that is searched for
 	 * @return
 	 */
-	bool doesPersonExist(const std::string &name) const;
+	bool doesPersonExist(const std::string& name) const;
 
 	/**
-	 * @brief Finds models that meet the given pattern
-	 * @param model_names
-	 * @param pattern
-	 * @return
+	 * @brief Evaluates whether the given @ref model_name matches any pattern given by @ref patterns
+	 *
+	 * @param model_name
+	 * @param patterns
+	 * @return std::tuple<bool, std::string>
 	 */
-	std::vector<std::string> findModels(const std::vector<std::string> &model_names, std::string pattern) const;
+	std::tuple<bool, std::string> isMatching(const std::string& model_name, std::vector<std::string>& patterns) const;
 
 	/**
 	 * @brief Get rid of the unnecessary model
 	 * @param model_names
 	 */
-	void deleteDestroyedPerson(const std::vector<std::string> &model_names);
+	void deleteDestroyedPerson(const std::vector<std::string>& model_names);
 
 	/**
-	 * @brief Executes rotation of the `vel` vector according to the given `transform`
-	 * @param vel: modified - rotated vector
-	 * @param transform
+	 * @brief Converts Gazebo ModelStates database to people representation in the @ref people_msgs_utils::People form
+	 *
+	 * @return people_msgs_utils::People
 	 */
-	void rotateTwist(geometry_msgs::TwistStamped &vel, const geometry_msgs::TransformStamped &transform);
+	people_msgs_utils::People gazeboModelsToPeople() const;
 
-	void publishPeople();
-	void publishPeoplePositions();
+	void publish();
+	void publishPeople(const people_msgs_utils::People& people);
+	void publishPeoplePositions(const people_msgs_utils::People& people);
 
 	ros::NodeHandle nh_;
 
@@ -79,20 +80,14 @@ private:
 	ros::Publisher pub_people_;
 	ros::Publisher pub_pos_;
 
-	std::vector<Person> people_data_;
-	/// @brief Algorithms looks for a model name that meets one of the given patterns
+	// key: name of the object in the simulation
+	// value: pair with object's ID and the Gazebo data structure
+	std::map<std::string, std::pair<size_t, gazebo_msgs::ModelState>> people_gazebo_;
+	/// @brief Algorithm looks for a model name that meets one of the given patterns
 	std::vector<std::string> people_name_patterns_;
-	/// @brief Stores name and ID correspondence of detected people
-	std::map<std::string, int> people_name_id_;
 
-	int id_next_;
-	std::string gazebo_tf_frame_;
+	std::string world_tf_frame_;
 	std::string target_tf_frame_;
-	int callback_counter_;
-	int callback_omits_;
-	unsigned long int seq_;
 
-	// possibly extend package features to:
-	// filtering detected obstacles - republishing ones indicating not-legs
-	friend class LegsExtraction;
+	ros::Timer timer_pub_;
 };
